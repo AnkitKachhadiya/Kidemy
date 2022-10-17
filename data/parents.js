911bd9e1e6718309d5a8a8b396c830db01000e75
96-malay <mshah95@stevens.edu>
2022-10-17 18:33:11 -0400

    Login functionality and checkParent

--- data/parents.js
+++ data/parents.js
@@ -10,150 +10,190 @@ const parents = mongoCollections.parents;
 const SALT_ROUNDS = 16;
 
 async function create(_firstName, _lastName, _email, _password) {
-    try {
-        validator.isSignUpTotalFieldsValid(arguments.length);
+  try {
+    validator.isSignUpTotalFieldsValid(arguments.length);
 
-        const firstName = validator.isFirstNameValid(xss(_firstName));
-        const lastName = validator.isLastNameValid(xss(_lastName));
-        const email = validator.isEmailValid(xss(_email));
-        const password = validator.isPasswordValid(xss(_password));
+    const firstName = validator.isFirstNameValid(xss(_firstName));
+    const lastName = validator.isLastNameValid(xss(_lastName));
+    const email = validator.isEmailValid(xss(_email));
+    const password = validator.isPasswordValid(xss(_password));
 
-        const parentsCollection = await parents();
+    const parentsCollection = await parents();
 
-        const parent = await parentsCollection.findOne({ email: email });
+    const parent = await parentsCollection.findOne({ email: email });
 
-        if (parent) {
-            throwError(
-                ErrorCode.BAD_REQUEST,
-                "Error: Already registered with given email id."
-            );
-        }
-
-        const passwordHash = await bcryptjs.hash(password, SALT_ROUNDS);
-
-        const newParentId = uuid.v4();
-        const verificationToken = uuid.v4();
-
-        const newParent = {
-            _id: newParentId,
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: passwordHash,
-            isVerified: false,
-            verificationToken: verificationToken,
-            children: [],
-        };
-
-        const insertedInfo = await parentsCollection.insertOne(newParent);
-
-        if (!insertedInfo.insertedId) {
-            throwError(
-                ErrorCode.INTERNAL_SERVER_ERROR,
-                "Error: Couldn't add parent."
-            );
-        }
-
-        return await get(newParentId);
-    } catch (error) {
-        throwCatchError(error);
+    if (parent) {
+      throwError(
+        ErrorCode.BAD_REQUEST,
+        "Error: Already registered with given email id."
+      );
     }
+
+    const passwordHash = await bcryptjs.hash(password, SALT_ROUNDS);
+
+    const newParentId = uuid.v4();
+    const verificationToken = uuid.v4();
+
+    const newParent = {
+      _id: newParentId,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: passwordHash,
+      isVerified: false,
+      verificationToken: verificationToken,
+      children: [],
+    };
+
+    const insertedInfo = await parentsCollection.insertOne(newParent);
+
+    if (!insertedInfo.insertedId) {
+      throwError(
+        ErrorCode.INTERNAL_SERVER_ERROR,
+        "Error: Couldn't add parent."
+      );
+    }
+
+    return await get(newParentId);
+  } catch (error) {
+    throwCatchError(error);
+  }
 }
 
 async function get(_newParentId) {
-    try {
-        validator.isGetParentTotalFieldsValid(arguments.length);
+  try {
+    validator.isGetParentTotalFieldsValid(arguments.length);
 
-        const parentId = validator.isParentIdValid(xss(_newParentId));
+    const parentId = validator.isParentIdValid(xss(_newParentId));
 
-        const parentsCollection = await parents();
+    const parentsCollection = await parents();
 
-        const parent = await parentsCollection.findOne(
-            { _id: parentId },
-            {
-                projection: {
-                    _id: 1,
-                    firstName: 1,
-                    lastName: 1,
-                    email: 1,
-                    isVerified: 1,
-                    verificationToken: 1,
-                },
-            }
-        );
+    const parent = await parentsCollection.findOne(
+      { _id: parentId },
+      {
+        projection: {
+          _id: 1,
+          firstName: 1,
+          lastName: 1,
+          email: 1,
+          isVerified: 1,
+          verificationToken: 1,
+        },
+      }
+    );
 
-        if (!parent) {
-            throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
-        }
-
-        return parent;
-    } catch (error) {
-        throwCatchError(error);
+    if (!parent) {
+      throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
     }
+
+    return parent;
+  } catch (error) {
+    throwCatchError(error);
+  }
 }
 
 async function verifyEmail(_parentId, _verificationToken) {
-    try {
-        const parentId = validator.isParentIdValid(xss(_parentId));
-        const verificationToken = validator.isVerificationTokenValid(
-            xss(_verificationToken)
-        );
+  try {
+    const parentId = validator.isParentIdValid(xss(_parentId));
+    const verificationToken = validator.isVerificationTokenValid(
+      xss(_verificationToken)
+    );
 
-        const parentsCollection = await parents();
+    const parentsCollection = await parents();
 
-        const parent = await parentsCollection.findOne(
-            { _id: parentId },
-            {
-                projection: {
-                    _id: 1,
-                    firstName: 1,
-                    lastName: 1,
-                    email: 1,
-                    isVerified: 1,
-                    verificationToken: 1,
-                },
-            }
-        );
+    const parent = await parentsCollection.findOne(
+      { _id: parentId },
+      {
+        projection: {
+          _id: 1,
+          firstName: 1,
+          lastName: 1,
+          email: 1,
+          isVerified: 1,
+          verificationToken: 1,
+        },
+      }
+    );
 
-        if (!parent) {
-            throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
-        }
-
-        if (parent.verificationToken !== verificationToken) {
-            throwError(
-                ErrorCode.NOT_FOUND,
-                "Error: Invalid verification token"
-            );
-        }
-
-        if (parent.isVerified) {
-            return { parentVerified: true };
-        }
-
-        const toBeUpdated = {
-            isVerified: true,
-        };
-
-        const updatedInfo = await parentsCollection.updateOne(
-            { _id: parentId },
-            { $set: toBeUpdated }
-        );
-
-        if (updatedInfo.modifiedCount !== 1) {
-            throwError(
-                ErrorCode.INTERNAL_SERVER_ERROR,
-                "Error: Could not verify parent."
-            );
-        }
-
-        return { parentVerified: true };
-    } catch (error) {
-        throwCatchError(error);
+    if (!parent) {
+      throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
     }
+
+    if (parent.verificationToken !== verificationToken) {
+      throwError(ErrorCode.NOT_FOUND, "Error: Invalid verification token");
+    }
+
+    if (parent.isVerified) {
+      return { parentVerified: true };
+    }
+
+    const toBeUpdated = {
+      isVerified: true,
+    };
+
+    const updatedInfo = await parentsCollection.updateOne(
+      { _id: parentId },
+      { $set: toBeUpdated }
+    );
+
+    if (updatedInfo.modifiedCount !== 1) {
+      throwError(
+        ErrorCode.INTERNAL_SERVER_ERROR,
+        "Error: Could not verify parent."
+      );
+    }
+
+    return { parentVerified: true };
+  } catch (error) {
+    throwCatchError(error);
+  }
 }
 
+async function checkParent(_email, _password) {
+  try {
+    validator.isCheckParentTotalFieldsValid(arguments.length);
+
+    const email = validator.isEmailValid(xss(_email));
+    const password = validator.isPasswordValid(xss(_password));
+
+    const parentsCollection = await parents();
+
+    const parent = await parentsCollection.findOne(
+      { email: email },
+      {
+        projection: {
+          _id: 1,
+          firstName: 1,
+          lastName: 1,
+          email: 1,
+          password: 1,
+          isVerified: 1,
+          verificationToken: 1,
+        },
+      }
+    );
+
+    if (!parent) {
+      throwError(ErrorCode.BAD_REQUEST, "Error: Incorrect email or password.");
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, parent.password);
+
+    if (!isPasswordCorrect) {
+      throwError(ErrorCode.BAD_REQUEST, "Error: Incorrect email or password.");
+    }
+
+    delete parent.password;
+
+    return parent;
+  } catch (error) {
+    throwCatchError(error);
+  }
+}
 
 module.exports = {
-    create,
-    get,
+  create,
+  get,
+  verifyEmail,
+  checkParent,
 };
--- routes/parents.js
+++ routes/parents.js
@@ -9,85 +9,139 @@ const nodemailer = require("nodemailer");
 const parentsData = data.parents;
 const router = express.Router();
 
-//signup form
-router.get("/signup", async (request, response) => {
-    if (request.session.parent) {
-        return response.redirect("/");
+//login form
+router.get("/", async (request, response) => {
+  if (request.session.parent) {
+    return response.redirect("/");
+  }
+
+  const signedUpFlashMessage = request.app.locals.isSignedUp
+    ? request.app.locals.signedUpFlashMessage
+    : false;
+
+  request.app.locals.isSignedUp = undefined;
+  request.app.locals.signedUpFlashMessage = undefined;
+
+  response.render("parents/login", {
+    pageTitle: "Login",
+    signedUpFlashMessage: signedUpFlashMessage,
+  });
+});
+
+//login submit
+router.post("/login", async (request, response) => {
+  if (request.session.parent) {
+    return response.redirect("/");
+  }
+
+  try {
+    const requestPostData = request.body;
+
+    validator.isLoginTotalFieldsValid(Object.keys(requestPostData).length);
+
+    const email = validator.isEmailValid(xss(requestPostData.email));
+    const password = validator.isPasswordValid(xss(requestPostData.password));
+
+    const parent = await parentsData.checkParent(email, password);
+
+    if (!parent) {
+      throwError(ErrorCode.INTERNAL_SERVER_ERROR, "Internal Server Error");
     }
 
-    response.render("parents/sign-up", { pageTitle: "Sign-up" });
+    request.session.parent = parent;
+
+    request.app.locals.isParentAuthenticated = true;
+
+    response.json({ isError: false });
+  } catch (error) {
+    response.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).json({
+      isError: true,
+      error: error.message || "Error: Internal server error.",
+    });
+  }
+});
+
+//logout
+router.get("/logout", async (request, response) => {
+  const parent = request.session.parent;
+
+  if (parent) {
+    request.session.destroy();
+    request.app.locals.isParentAuthenticated = false;
+  }
+
+  response.redirect("/parents");
+});
+
+//signup form
+router.get("/signup", async (request, response) => {
+  if (request.session.parent) {
+    return response.redirect("/");
+  }
+
+  response.render("parents/sign-up", { pageTitle: "Sign-up" });
 });
 
 //signup submit
 router.post("/signup", async (request, response) => {
-    if (request.session.parent) {
-        return response.redirect("/");
+  if (request.session.parent) {
+    return response.redirect("/");
+  }
+
+  try {
+    const requestPostData = request.body;
+
+    validator.isSignUpTotalFieldsValid(Object.keys(requestPostData).length);
+
+    const firstName = validator.isFirstNameValid(
+      xss(requestPostData.firstName)
+    );
+    const lastName = validator.isLastNameValid(xss(requestPostData.lastName));
+    const email = validator.isEmailValid(xss(requestPostData.email));
+    const password = validator.isPasswordValid(xss(requestPostData.password));
+
+    const parent = await parentsData.create(
+      firstName,
+      lastName,
+      email,
+      password
+    );
+
+    if (!parent) {
+      throwError(ErrorCode.INTERNAL_SERVER_ERROR, "Internal Server Error");
     }
 
-    try {
-        const requestPostData = request.body;
+    request.app.locals.isSignedUp = true;
+    request.app.locals.signedUpFlashMessage =
+      "Signed up successfully. Login to start using Kidemy.";
 
-        validator.isSignUpTotalFieldsValid(Object.keys(requestPostData).length);
-
-        const firstName = validator.isFirstNameValid(
-            xss(requestPostData.firstName)
-        );
-        const lastName = validator.isLastNameValid(
-            xss(requestPostData.lastName)
-        );
-        const email = validator.isEmailValid(xss(requestPostData.email));
-        const password = validator.isPasswordValid(
-            xss(requestPostData.password)
-        );
-
-        const parent = await parentsData.create(
-            firstName,
-            lastName,
-            email,
-            password
-        );
-
-        if (!parent) {
-            throwError(
-                ErrorCode.INTERNAL_SERVER_ERROR,
-                "Internal Server Error"
-            );
-        }
-
-        request.app.locals.isSignedUp = true;
-        request.app.locals.signedUpFlashMessage =
-            "Signed up successfully. Login to start using Kidemy.";
-
-        response.json({ isError: false });
-    } catch (error) {
-        response.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).json({
-            isError: true,
-            error: error.message || "Error: Internal server error.",
-        });
-    }
+    response.json({ isError: false });
+  } catch (error) {
+    response.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).json({
+      isError: true,
+      error: error.message || "Error: Internal server error.",
+    });
+  }
 });
 
 //sending verification email
 router.post("/sendVerificationEmail", async (request, response) => {
-    if (!request.session.parent) {
-        return response.redirect("/");
+  if (!request.session.parent) {
+    return response.redirect("/");
+  }
+
+  try {
+    const parent = await parentsData.get(request.session.parent._id);
+
+    if (!parent) {
+      throwError(ErrorCode.INTERNAL_SERVER_ERROR, "Internal Server Error");
     }
 
-    try {
-        const parent = await parentsData.get(request.session.parent._id);
+    if (parent.isVerified) {
+      return response.redirect("/");
+    }
 
-        if (!parent) {
-            throwError(
-                ErrorCode.INTERNAL_SERVER_ERROR,
-                "Internal Server Error"
-            );
-        }
-
-        if (parent.isVerified) {
-            return response.redirect("/");
-        }
-
-        const emailTemplate = `
+    const emailTemplate = `
         Hi <strong>${parent.firstName} ${parent.lastName}</strong>,
         <br>
         <p>We're happy you signed up for Kidemy. To start exploring the Kidemy, please confirm your email address.</p>
@@ -95,40 +149,40 @@ router.post("/sendVerificationEmail", async (request, response) => {
         <p>Welcome to Kidemy! <br> The Kidemy Team</p>
         `;
 
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.USER,
-                pass: process.env.PASSWORD,
-            },
-        });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASSWORD,
+      },
+    });
 
-        const mailOptions = {
-            from: process.env.USER,
-            to: parent.email,
-            subject: "Kidemy account verification email",
-            html: emailTemplate,
-        };
+    const mailOptions = {
+      from: process.env.USER,
+      to: parent.email,
+      subject: "Kidemy account verification email",
+      html: emailTemplate,
+    };
 
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log("Email Sent");
-            }
-        });
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email Sent");
+      }
+    });
 
-        response.json({ isError: false });
-    } catch (error) {
-        response.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).json({
-            isError: true,
-            error: error.message || "Error: Internal server error.",
-        });
-    }
+    response.json({ isError: false });
+  } catch (error) {
+    response.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).json({
+      isError: true,
+      error: error.message || "Error: Internal server error.",
+    });
+  }
 });
 
 const throwError = (code = 500, message = "Error: Internal Server Error") => {
-    throw { code, message };
+  throw { code, message };
 };
 
 module.exports = router;

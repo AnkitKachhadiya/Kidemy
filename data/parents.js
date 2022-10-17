248f9a943d8b74632835c6147d220ae8979cfba9
Mehul Savaliya <mehulsavaliya520@gmail.com>
2022-10-17 18:25:44 -0400

    Email Verification

--- data/parents.js
+++ data/parents.js
@@ -92,6 +92,67 @@ async function get(_newParentId) {
     }
 }
 
+async function verifyEmail(_parentId, _verificationToken) {
+    try {
+        const parentId = validator.isParentIdValid(xss(_parentId));
+        const verificationToken = validator.isVerificationTokenValid(
+            xss(_verificationToken)
+        );
+
+        const parentsCollection = await parents();
+
+        const parent = await parentsCollection.findOne(
+            { _id: parentId },
+            {
+                projection: {
+                    _id: 1,
+                    firstName: 1,
+                    lastName: 1,
+                    email: 1,
+                    isVerified: 1,
+                    verificationToken: 1,
+                },
+            }
+        );
+
+        if (!parent) {
+            throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
+        }
+
+        if (parent.verificationToken !== verificationToken) {
+            throwError(
+                ErrorCode.NOT_FOUND,
+                "Error: Invalid verification token"
+            );
+        }
+
+        if (parent.isVerified) {
+            return { parentVerified: true };
+        }
+
+        const toBeUpdated = {
+            isVerified: true,
+        };
+
+        const updatedInfo = await parentsCollection.updateOne(
+            { _id: parentId },
+            { $set: toBeUpdated }
+        );
+
+        if (updatedInfo.modifiedCount !== 1) {
+            throwError(
+                ErrorCode.INTERNAL_SERVER_ERROR,
+                "Error: Could not verify parent."
+            );
+        }
+
+        return { parentVerified: true };
+    } catch (error) {
+        throwCatchError(error);
+    }
+}
+
+
 module.exports = {
     create,
     get,
--- routes/parents.js
+++ routes/parents.js
@@ -67,6 +67,66 @@ router.post("/signup", async (request, response) => {
     }
 });
 
+//sending verification email
+router.post("/sendVerificationEmail", async (request, response) => {
+    if (!request.session.parent) {
+        return response.redirect("/");
+    }
+
+    try {
+        const parent = await parentsData.get(request.session.parent._id);
+
+        if (!parent) {
+            throwError(
+                ErrorCode.INTERNAL_SERVER_ERROR,
+                "Internal Server Error"
+            );
+        }
+
+        if (parent.isVerified) {
+            return response.redirect("/");
+        }
+
+        const emailTemplate = `
+        Hi <strong>${parent.firstName} ${parent.lastName}</strong>,
+        <br>
+        <p>We're happy you signed up for Kidemy. To start exploring the Kidemy, please confirm your email address.</p>
+        <a href="http://localhost:3000/parents/verifyEmail/${parent._id}&${parent.verificationToken}" target="_blank">Verify Email</a>
+        <p>Welcome to Kidemy! <br> The Kidemy Team</p>
+        `;
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.USER,
+            to: parent.email,
+            subject: "Kidemy account verification email",
+            html: emailTemplate,
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Email Sent");
+            }
+        });
+
+        response.json({ isError: false });
+    } catch (error) {
+        response.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).json({
+            isError: true,
+            error: error.message || "Error: Internal server error.",
+        });
+    }
+});
+
 const throwError = (code = 500, message = "Error: Internal Server Error") => {
     throw { code, message };
 };

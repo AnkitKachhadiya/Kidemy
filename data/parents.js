be127c54ad3d2f0a7b08a7dba45033a68ad407b2
96-malay <mshah95@stevens.edu>
2022-10-31 15:42:50 -0400

    testing email validation

--- data/parents.js
+++ data/parents.js
@@ -10,272 +10,305 @@ const parents = mongoCollections.parents;
 const SALT_ROUNDS = 16;
 
 async function create(_firstName, _lastName, _email, _password) {
-    try {
-        validator.isSignUpTotalFieldsValid(arguments.length);
+  try {
+    validator.isSignUpTotalFieldsValid(arguments.length);
 
-        const firstName = validator.isFirstNameValid(xss(_firstName));
-        const lastName = validator.isLastNameValid(xss(_lastName));
-        const email = validator.isEmailValid(xss(_email));
-        const password = validator.isPasswordValid(xss(_password));
+    const firstName = validator.isFirstNameValid(xss(_firstName));
+    const lastName = validator.isLastNameValid(xss(_lastName));
+    const email = validator.isEmailValid(xss(_email));
+    const password = validator.isPasswordValid(xss(_password));
 
-        const parentsCollection = await parents();
+    const parentsCollection = await parents();
 
-        const parent = await parentsCollection.findOne({ email: email });
+    const parent = await parentsCollection.findOne({ email: email });
 
-        if (parent) {
-            throwError(
-                ErrorCode.BAD_REQUEST,
-                "Error: Already registered with given email id."
-            );
-        }
-
-        const passwordHash = await bcryptjs.hash(password, SALT_ROUNDS);
-
-        const newParentId = uuid.v4();
-        const verificationToken = uuid.v4();
-
-        const newParent = {
-            _id: newParentId,
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: passwordHash,
-            isVerified: false,
-            verificationToken: verificationToken,
-            children: [],
-        };
-
-        const insertedInfo = await parentsCollection.insertOne(newParent);
-
-        if (!insertedInfo.insertedId) {
-            throwError(
-                ErrorCode.INTERNAL_SERVER_ERROR,
-                "Error: Couldn't add parent."
-            );
-        }
-
-        return await get(newParentId);
-    } catch (error) {
-        throwCatchError(error);
+    if (parent) {
+      throwError(
+        ErrorCode.BAD_REQUEST,
+        "Error: Already registered with given email id."
+      );
     }
+
+    const passwordHash = await bcryptjs.hash(password, SALT_ROUNDS);
+
+    const newParentId = uuid.v4();
+    const verificationToken = uuid.v4();
+
+    const newParent = {
+      _id: newParentId,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: passwordHash,
+      isVerified: false,
+      verificationToken: verificationToken,
+      children: [],
+    };
+
+    const insertedInfo = await parentsCollection.insertOne(newParent);
+
+    if (!insertedInfo.insertedId) {
+      throwError(
+        ErrorCode.INTERNAL_SERVER_ERROR,
+        "Error: Couldn't add parent."
+      );
+    }
+
+    return await get(newParentId);
+  } catch (error) {
+    throwCatchError(error);
+  }
 }
 
 async function get(_newParentId) {
-    try {
-        validator.isGetParentTotalFieldsValid(arguments.length);
+  try {
+    validator.isGetParentTotalFieldsValid(arguments.length);
 
-        const parentId = validator.isParentIdValid(xss(_newParentId));
+    const parentId = validator.isParentIdValid(xss(_newParentId));
 
-        const parentsCollection = await parents();
+    const parentsCollection = await parents();
 
-        const parent = await parentsCollection.findOne(
-            { _id: parentId },
-            {
-                projection: {
-                    _id: 1,
-                    firstName: 1,
-                    lastName: 1,
-                    email: 1,
-                    isVerified: 1,
-                    verificationToken: 1,
-                },
-            }
-        );
+    const parent = await parentsCollection.findOne(
+      { _id: parentId },
+      {
+        projection: {
+          _id: 1,
+          firstName: 1,
+          lastName: 1,
+          email: 1,
+          isVerified: 1,
+          verificationToken: 1,
+        },
+      }
+    );
 
-        if (!parent) {
-            throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
-        }
-
-        return parent;
-    } catch (error) {
-        throwCatchError(error);
+    if (!parent) {
+      throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
     }
+
+    return parent;
+  } catch (error) {
+    throwCatchError(error);
+  }
 }
 
 async function verifyEmail(_parentId, _verificationToken) {
-    try {
-        const parentId = validator.isParentIdValid(xss(_parentId));
-        const verificationToken = validator.isVerificationTokenValid(
-            xss(_verificationToken)
-        );
+  try {
+    const parentId = validator.isParentIdValid(xss(_parentId));
+    const verificationToken = validator.isVerificationTokenValid(
+      xss(_verificationToken)
+    );
 
-        const parentsCollection = await parents();
+    const parentsCollection = await parents();
 
-        const parent = await parentsCollection.findOne(
-            { _id: parentId },
-            {
-                projection: {
-                    _id: 1,
-                    firstName: 1,
-                    lastName: 1,
-                    email: 1,
-                    isVerified: 1,
-                    verificationToken: 1,
-                },
-            }
-        );
+    const parent = await parentsCollection.findOne(
+      { _id: parentId },
+      {
+        projection: {
+          _id: 1,
+          firstName: 1,
+          lastName: 1,
+          email: 1,
+          isVerified: 1,
+          verificationToken: 1,
+        },
+      }
+    );
 
-        if (!parent) {
-            throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
-        }
-
-        if (parent.verificationToken !== verificationToken) {
-            throwError(
-                ErrorCode.NOT_FOUND,
-                "Error: Invalid verification token"
-            );
-        }
-
-        if (parent.isVerified) {
-            return { parentVerified: true };
-        }
-
-        const toBeUpdated = {
-            isVerified: true,
-        };
-
-        const updatedInfo = await parentsCollection.updateOne(
-            { _id: parentId },
-            { $set: toBeUpdated }
-        );
-
-        if (updatedInfo.modifiedCount !== 1) {
-            throwError(
-                ErrorCode.INTERNAL_SERVER_ERROR,
-                "Error: Could not verify parent."
-            );
-        }
-
-        return { parentVerified: true };
-    } catch (error) {
-        throwCatchError(error);
+    if (!parent) {
+      throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
     }
+
+    if (parent.verificationToken !== verificationToken) {
+      throwError(ErrorCode.NOT_FOUND, "Error: Invalid verification token");
+    }
+
+    if (parent.isVerified) {
+      return { parentVerified: true };
+    }
+
+    const toBeUpdated = {
+      isVerified: true,
+    };
+
+    const updatedInfo = await parentsCollection.updateOne(
+      { _id: parentId },
+      { $set: toBeUpdated }
+    );
+
+    if (updatedInfo.modifiedCount !== 1) {
+      throwError(
+        ErrorCode.INTERNAL_SERVER_ERROR,
+        "Error: Could not verify parent."
+      );
+    }
+
+    return { parentVerified: true };
+  } catch (error) {
+    throwCatchError(error);
+  }
 }
 
 async function checkParent(_email, _password) {
-    try {
-        validator.isCheckParentTotalFieldsValid(arguments.length);
+  try {
+    validator.isCheckParentTotalFieldsValid(arguments.length);
 
-        const email = validator.isEmailValid(xss(_email));
-        const password = validator.isPasswordValid(xss(_password));
+    const email = validator.isEmailValid(xss(_email));
+    const password = validator.isPasswordValid(xss(_password));
 
-        const parentsCollection = await parents();
+    const parentsCollection = await parents();
 
-        const parent = await parentsCollection.findOne(
-            { email: email },
-            {
-                projection: {
-                    _id: 1,
-                    firstName: 1,
-                    lastName: 1,
-                    email: 1,
-                    password: 1,
-                    isVerified: 1,
-                    verificationToken: 1,
-                },
-            }
-        );
+    const parent = await parentsCollection.findOne(
+      { email: email },
+      {
+        projection: {
+          _id: 1,
+          firstName: 1,
+          lastName: 1,
+          email: 1,
+          password: 1,
+          isVerified: 1,
+          verificationToken: 1,
+        },
+      }
+    );
 
-        if (!parent) {
-            throwError(
-                ErrorCode.BAD_REQUEST,
-                "Error: Incorrect email or password."
-            );
-        }
-
-        const isPasswordCorrect = await bcryptjs.compare(
-            password,
-            parent.password
-        );
-
-        if (!isPasswordCorrect) {
-            throwError(
-                ErrorCode.BAD_REQUEST,
-                "Error: Incorrect email or password."
-            );
-        }
-
-        delete parent.password;
-
-        return parent;
-    } catch (error) {
-        throwCatchError(error);
+    if (!parent) {
+      throwError(ErrorCode.BAD_REQUEST, "Error: Incorrect email or password.");
     }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, parent.password);
+
+    if (!isPasswordCorrect) {
+      throwError(ErrorCode.BAD_REQUEST, "Error: Incorrect email or password.");
+    }
+
+    delete parent.password;
+
+    return parent;
+  } catch (error) {
+    throwCatchError(error);
+  }
 }
 
 async function addChild(_parentId, _firstName, _lastName, _email, _password) {
-    try {
-        const parentId = validator.isParentIdValid(xss(_parentId));
-        const firstName = validator.isFirstNameValid(xss(_firstName));
-        const lastName = validator.isLastNameValid(xss(_lastName));
-        const email = validator.isEmailValid(xss(_email));
-        const password = validator.isPasswordValid(xss(_password));
+  try {
+    const parentId = validator.isParentIdValid(xss(_parentId));
+    const firstName = validator.isFirstNameValid(xss(_firstName));
+    const lastName = validator.isLastNameValid(xss(_lastName));
+    const email = validator.isEmailValid(xss(_email));
+    const password = validator.isPasswordValid(xss(_password));
 
-        const parentsCollection = await parents();
+    const parentsCollection = await parents();
 
-        const user = await parentsCollection.findOne({
-            $or: [
-                { email: email },
-                {
-                    "children.email": email,
-                },
-            ],
-        });
+    const user = await parentsCollection.findOne({
+      $or: [
+        { email: email },
+        {
+          "children.email": email,
+        },
+      ],
+    });
 
-        if (user) {
-            throwError(
-                ErrorCode.BAD_REQUEST,
-                "Error: Already registered with given email id."
-            );
-        }
-
-        const passwordHash = await bcryptjs.hash(password, SALT_ROUNDS);
-
-        const newChild = {
-            _id: uuid.v4(),
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: passwordHash,
-            courses: [],
-        };
-
-        const updatedInfo = await parentsCollection.updateOne(
-            { _id: parentId },
-            { $push: { children: newChild } }
-        );
-
-        if (updatedInfo.modifiedCount !== 1) {
-            throwError(
-                ErrorCode.INTERNAL_SERVER_ERROR,
-                "Error: Could not create child account."
-            );
-        }
-
-        return { accountCreated: true };
-    } catch (error) {
-        throwCatchError(error);
+    if (user) {
+      throwError(
+        ErrorCode.BAD_REQUEST,
+        "Error: Already registered with given email id."
+      );
     }
+
+    const passwordHash = await bcryptjs.hash(password, SALT_ROUNDS);
+
+    const newChild = {
+      _id: uuid.v4(),
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: passwordHash,
+      courses: [],
+    };
+
+    const updatedInfo = await parentsCollection.updateOne(
+      { _id: parentId },
+      { $push: { children: newChild } }
+    );
+
+    if (updatedInfo.modifiedCount !== 1) {
+      throwError(
+        ErrorCode.INTERNAL_SERVER_ERROR,
+        "Error: Could not create child account."
+      );
+    }
+
+    return { accountCreated: true };
+  } catch (error) {
+    throwCatchError(error);
+  }
 }
 
 const throwError = (code = 500, message = "Error: Internal Server Error") => {
-    throw { code, message };
+  throw { code, message };
 };
 
 const throwCatchError = (error) => {
-    if (error.code && error.message) {
-        throwError(error.code, error.message);
-    }
+  if (error.code && error.message) {
+    throwError(error.code, error.message);
+  }
 
-    throwError(
-        ErrorCode.INTERNAL_SERVER_ERROR,
-        "Error: Internal server error."
-    );
+  throwError(ErrorCode.INTERNAL_SERVER_ERROR, "Error: Internal server error.");
 };
 
+async function updateProfile(_parentId, _firstName, _lastName) {
+  try {
+    const parentId = validator.isParentIdValid(xss(_parentId));
+    const firstName = validator.isFirstNameValid(xss(_firstName));
+    const lastName = validator.isLastNameValid(xss(_lastName));
+
+    const parentsCollection = await parents();
+
+    const parent = await parentsCollection.findOne(
+      { _id: parentId },
+      {
+        projection: {
+          _id: 1,
+        },
+      }
+    );
+
+    if (!parent) {
+      throwError(ErrorCode.NOT_FOUND, "Error: Parent not found.");
+    }
+
+    const toBeUpdated = {
+      firstName: firstName,
+      lastName: lastName,
+    };
+
+    const updatedInfo = await parentsCollection.updateOne(
+      { _id: parentId },
+      { $set: toBeUpdated }
+    );
+
+    if (updatedInfo.modifiedCount !== 1) {
+      throwError(
+        ErrorCode.INTERNAL_SERVER_ERROR,
+        "Error: Could not update profile."
+      );
+    }
+
+    return { profileUpdated: true };
+  } catch (error) {
+    throwCatchError(error);
+  }
+}
+
 module.exports = {
-    create,
-    get,
-    verifyEmail,
-    checkParent,
+  create,
+  get,
+  checkParent,
+  verifyEmail,
+  addChild,
+  updateProfile,
+  getChild,
+  updateChild,
 };

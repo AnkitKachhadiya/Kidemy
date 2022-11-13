2d9b6a6efafde37e261086597a403accf2ec187c
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2022-11-12 20:21:59 -0500

    formatting

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,27 @@ const coursesRoutes = require("./courses");
 const path = require("path");
 
 const constructorMethod = (app) => {
-  app.use("/parents", parentsRoutes);
-  app.use("/children", childrenRoutes);
-  app.use("/courses", coursesRoutes);
+    app.use("/parents", parentsRoutes);
+    app.use("/children", childrenRoutes);
+    app.use("/courses", coursesRoutes);
 
-  app.get("/", (request, response) => {
-    return response.render("home", { pageTitle: "Home" });
-  });
+    app.get("/", (request, response) => {
+        return response.render("home", { pageTitle: "Home" });
+    });
 
-  //for accessing unknown routes
-  app.use("*", (request, response) => {
-    response.status(404).sendFile(path.resolve("static/page-not-found.html"));
-  });
+    //for accessing unknown routes
+    app.use("*", (request, response) => {
+        response
+            .status(404)
+            .sendFile(path.resolve("static/page-not-found.html"));
+    });
 
-  //for invalid URI
-  app.use(function (error, request, response, next) {
-    response.status(404).sendFile(path.resolve("static/page-not-found.html"));
-  });
+    //for invalid URI
+    app.use(function (error, request, response, next) {
+        response
+            .status(404)
+            .sendFile(path.resolve("static/page-not-found.html"));
+    });
 };
 
 module.exports = constructorMethod;

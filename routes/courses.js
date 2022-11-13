4e87c9d8579205088bead5cbcd4b92aa1cd137e7
96-malay <mshah95@stevens.edu>
2022-11-12 20:10:57 -0500

    Courses Route

---
+++ routes/courses.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const validator = require("../helpers/validator");
+const xss = require("xss");
+const ErrorCode = require("../helpers/error-code");
+const data = require("../data");
+
+const coursesData = data.courses;
+const parentsData = data.parents;
+const router = express.Router();
+
+router.get("/", async (request, response) => {
+    const courses = await coursesData.getAllCourses();
+
+    response.render("courses/all", {
+        pageTitle: "All Courses",
+        courses: courses,
+    });
+});
+
+router.get("/:id", async (request, response) => {
+    const course = await coursesData.get(xss(request.params.id));
+
+    console.log(course);
+
+    const courseAnalytics = {
+        totalVideos: 0,
+        totalQuizzes: 0,
+    };
+
+    for (const currentModule of course.modules) {
+        if (currentModule.type === "Video") {
+            courseAnalytics.totalVideos++;
+        }
+
+        if (currentModule.type === "Quiz") {
+            courseAnalytics.totalQuizzes++;
+        }
+    }
+
+    courseAnalytics.totalModules = course.modules.length;
+
+    response.render("courses/details", {
+        pageTitle: "Course",
+        course: course,
+        courseAnalytics: courseAnalytics,
+    });
+});
+
+module.exports = router;
